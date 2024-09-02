9d5f3b2ab609e7fe2448b09f7a51792dfca1315a
Parashar Das <porashorbabu@gmail.com>
2024-09-02 14:32:33 +0600

    testing project init successfully

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import Chart2 from './components/Chart2'
+
+const App = () => {
+  return (
+    <div className='text-xl bg-green-500'>
+      <Chart2/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Chart2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chart2 = () => {
+  return (
+    <div>
+      this is chart field
+    </div>
+  )
+}
+
+export default Chart2
